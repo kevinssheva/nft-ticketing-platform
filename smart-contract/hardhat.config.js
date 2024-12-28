97142a681806f8267d7e1dcf31a1f83a729e7a36
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2024-12-28 17:10:08 +0700

    Feat: create private network using kurtosis geth

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -4,8 +4,13 @@ require('@nomicfoundation/hardhat-toolbox');
 module.exports = {
   solidity: '0.8.28',
   networks: {
-    hardhat: {
-      chainId: 31337,
-    },
+    localnet: {
+      url: 'http://127.0.0.1:26802',
+      chainId: 585858,
+      accounts: [
+        'c5114526e042343c6d1899cad05e1c00ba588314de9b96929914ee0df18d46b2',
+        'a492823c3e193d6c595f37a18e3c06650cf4c74558cc818b16130b293716106f'
+      ]
+    }
   },
 };
--- smart-contract/ignition/modules/TicketMarketplace.js
+++ smart-contract/ignition/modules/TicketMarketplace.js
@@ -3,7 +3,7 @@ const { ethers } = require('hardhat');
 
 module.exports = buildModule('TicketModule', (m) => {
   const adminAddress = ethers.getAddress(
-    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+    '0xD9211042f35968820A3407ac3d80C725f8F75c14'
   );
   // Parameters
   const ticketPrice = m.getParameter('ticketPrice', ethers.parseEther('0.1')); // Default 0.1 ETH
